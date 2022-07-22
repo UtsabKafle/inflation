9d3a2ce55f311bcdfc1c6d289aee44338178864f
utsab kafle <utsabcodes@gmail.com>
2022-07-22 10:53:54 +0545

    worked on expenses page and few others

--- frontend/src/Components/Expenses/Expenses.jsx
+++ frontend/src/Components/Expenses/Expenses.jsx
@@ -23,11 +23,56 @@ const Expenses = () => {
       </div>
 
       <div className="right">
+        <div className="cir-cont">
+          <div className="green">
+            <div id="cir-text">+200</div>
+            <div className="cir-details">
+              <div className="cir-date">2020-02-04</div>
+              <div className="cir-text">Some important work was to be done</div>
+            </div>
+          </div>
+        </div>
+        <div className="cir-cont">
+          <div className="line"></div>
+          <div className="red">
+          <div id="cir-text">-300</div>
+            <div className="cir-details">
+              <div className="cir-date">2020-02-07</div>
+              <div className="cir-text">Some useless work was to be done</div>
+            </div>
+          </div>
+        </div>
+        <div className="cir-cont">
+          <div className="line"></div>
+          <div className="green">
+          <div id="cir-text">+200</div>
+            <div className="cir-details">
+              <div className="cir-date">2020-02-09</div>
+              <div className="cir-text">Some crap work was to be done</div>
+            </div>
+          </div>
+        </div>
+        <div className="cir-cont">
+          <div className="line"></div>
+          <div className="green">
+          <div id="cir-text">+2500</div>
+            <div className="cir-details">
+              <div className="cir-date">2020-02-11</div>
+              <div className="cir-text">Some shitty work was to be done because I was busy in london</div>
+            </div>
+          </div>
+        </div>
+        <div className="cir-cont">
+          <div className="line"></div>
+          <div className="red">
+          <div id="cir-text">-400</div>
+            <div className="cir-details">
+              <div className="cir-date">2020-02-04</div>
+              <div className="cir-text">Some important work was to be done</div>
+            </div>
+          </div>
+        </div>
 
-        <div className="green">+200</div>
-        <div className="red">-300</div>
-        <div className="green">+4000</div>
-   
       </div>
 
     </div>
--- frontend/src/Components/home/home.jsx
+++ frontend/src/Components/home/home.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import './Styles/home.css'
 import Expenses from '../Expenses/Expenses'
 import Sidebar from '../Sidebar/Sidebar'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Earnings from '../Earnings/Earnings'
+import Investments from '../Investments/Investments';
 const home = () => {
   return (
     <div>
 
           <BrowserRouter>
           <Routes>
-
-            <Route path='expenses' element={<Expenses/>}/>
+        <Route path='investments' element={<Investments/>}/>
+        <Route path='expenses' element={<Expenses/>}/>
+        <Route path='earnings' element={<Earnings/>}/>
             <Route path='' element={<h2>Daashboard</h2>}/>
           </Routes>    
           <Sidebar/>
